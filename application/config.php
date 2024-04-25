4105ef679e66de00885d0c386cbfa3b569a38545
contatoprodutivo <contatoprodutivo@hotmail.com>
2024-04-25 00:55:32 -0300

    Alteracões

--- application/config.php
+++ application/config.php
@@ -1,7 +1,7 @@
 <?php
-$db_host	    = 'localhost';
-$db_user        = 'u271859541_crmtransnatal';
-$db_password	= '~Ad4]OzW/';
-$db_name	    = 'u271859541_crmtransnatal';
-define('APP_URL', 'https://crmtransnatal.contatoprodutivo.com.br');
-$_app_stage = 'Live'; // You can set this variable Live to Dev to enable ibilling Debug
+$db_host      = getenv('DB_HOST') ?: 'localhost';
+$db_user      = getenv('DB_USER') ?: 'root';
+$db_password  = getenv('DB_PASSWORD') ?: '';
+$db_name      = getenv('DB_NAME') ?: 'crmtransnatal';
+$app_url      = getenv('APP_URL') ?: 'http://localhost/crmtransnatal';
+$_app_stage   = getenv('APP_STAGE') ?: 'Dev'; // Default to Dev to enable debugging
