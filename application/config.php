8b99cb664431a2290276b8ef297149248ed4785e
contatoprodutivo <contatoprodutivo@hotmail.com>
2024-04-25 00:38:25 -0300

    Remove config.php from version control

--- application/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$db_host	    = 'localhost';
-$db_user        = 'u271859541_crmtransnatal';
-$db_password	= '~Ad4]OzW/';
-$db_name	    = 'u271859541_crmtransnatal';
-define('APP_URL', 'https://crmtransnatal.contatoprodutivo.com.br');
-$_app_stage = 'Live'; // You can set this variable Live to Dev to enable ibilling Debug
